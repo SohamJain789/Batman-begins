749d732410c89b6f0fad08543e3a029894b9839b
SohamJain789 <69146549+SohamJain789@users.noreply.github.com>
2020-10-19 00:13:06 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 var boyImg,boy;
+var drop;
 
 function preload(){
 boyImg = loadAnimation("walking_1.png","walking_2.png","walking_3.png","walking_4.png","walking_5.png","walking_6.png","walking_7.png","walking_8.png");
@@ -7,12 +8,14 @@ boyImg = loadAnimation("walking_1.png","walking_2.png","walking_3.png","walking_
 function setup(){
  createCanvas(300,400)
  boy = createSprite(150,350,30,30)
-    
+ boy.addAnimation("boy",boyImg);
+ boy.scale = 0.15;
+ 
+ drop = new Rain(150,0)
 }
 
 function draw(){
 background(0,0,0)
-boy.addAnimation("boy",boyImg);
-boy.scale = 0.14;
+
 drawSprites();
 }   
